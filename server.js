c8a3657febfbec8c4f03361f5056ab04ba7e2dc0
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-06-11 20:06:12 -0700

    Fixing file paths

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const noteData = require('./Develop/db/db.json');
-const uuid = require('./Develop/db/helpers/uuid');
+const noteData = require('./db/db.json');
+const uuid = require('./db/helpers/uuid');
 const PORT = 3001;
 
 const app = express();
