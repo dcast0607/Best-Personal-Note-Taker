664b3c1ad51139a5a2f75df23e9fa0cce7706570
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-04 19:49:16 -0700

    Commenting the uuid.js file

--- db/helpers/uuid.js
+++ db/helpers/uuid.js
@@ -1,4 +1,8 @@
 // Immediately export a function that generates a string of random numbers and letters
+
+// The purpose of this function is to generate a random number so that it can be used
+// as an ID that is linked to our note. In addition to this, the ID that is generated
+// from this uuid.js will be used to validate existing notes.
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
